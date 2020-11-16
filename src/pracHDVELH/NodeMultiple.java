17e56542608600038446071b615207f738954bef
Zekri <adham.zekri@etu.univ-amu.fr>
2020-11-16 12:20:47 +0100

    zekri adham

--- src/pracHDVELH/NodeMultiple.java
+++ src/pracHDVELH/NodeMultiple.java
@@ -4,7 +4,6 @@
  * Template étudiants
  */
 package pracHDVELH;
-
 import myUtils.ErrorNaiveHandler;
 
 /**
@@ -15,24 +14,32 @@ public class NodeMultiple {
 	public static final int ERROR_STATUS_INDEX_OUT_OF_RANGE = -1;
 	public static final String ERROR_MSG_INDEX_OUT_OF_RANGE = "Index out of range";
 	public static int NODE_MAX_ARITY = 10;
+	public static NodeMultiple[] daughters = new NodeMultiple[NODE_MAX_ARITY];
+	public static Object data;
 
+	/* Getters/Setters */
 	/* Overridden methods */
 	@Override
 	public String toString() {
-		/* TO BE COMPLETED */
+		return data.toString();
 	}
-
-	/* Getters/Setters */
 	/**
 	 * Gets the {@code i}th daughter node.
-	 * 
+	 * <p>
 	 * Aborts if the given index {@code i} is out of range.
-	 * 
+	 *
 	 * @param i the index of the daughter node.
 	 * @return the {@code i}th daughter node, or {@code null} if it does not exist.
 	 */
 	public NodeMultiple getDaughter(int i) {
-		/* TO BE COMPLETED */
+		if (daughters == null) return;
+		int i = 0;
+		while (daughters[i] != null && i < NODE_MAX_ARITY) {
+			++i;
+		}
+		if (i < NODE_MAX_ARITY)
+			daughters[i] = daughters;
+		return daughters[i];
 	}
 
 	/**
@@ -40,68 +47,76 @@ public class NodeMultiple {
 	 * Should be used cautiously, since {@code i} may not be the first index
 	 * available (i.e. there may be lower indexes which do not refer to any
 	 * daughter).
-	 * 
+	 * <p>
 	 * If a daughter node is already referred to at this index then it is erased
 	 * with {@code daughter}.
-	 * 
+	 * <p>
 	 * Aborts if the index {@code i} is out of range.
-	 * 
+	 *
 	 * @param daughter the node to be linked as a daughter of {@code this} node.
 	 * @param i        the daughter node's index
 	 */
 	public void setDaughter(NodeMultiple daughter, int i) {
-		/* TO BE COMPLETED */
+		if (i >= NODE_MAX_ARITY)
+			return;
+		daughters[i] = daughter;
 	}
 
 	/**
 	 * @return all the daughters
 	 */
 	public NodeMultiple[] getDaughters() {
-		/* TO BE COMPLETED */
+		return daughters;
 	}
 
 	/**
 	 * @param daughters the daughters to set
 	 */
 	public void setDaughters(NodeMultiple[] daughters) {
-		/* TO BE COMPLETED */
+		this->daughters = daughters;
 	}
 
 	/**
 	 * Adds the given {@code daughter} node at the first available index.
-	 * 
+	 * <p>
 	 * If the max number of daughters ({@link #NODE_MAX_ARITY}) is already reached
 	 * nothing happens (no abort).
-	 * 
+	 *
 	 * @param daughter
 	 */
 	public void addDaughter(NodeMultiple daughter) {
-		/* TO BE COMPLETED */
+		for (int i = 0; i < NODE_MAX_ARITY; ++i)
+			if (!daughters[i])
+				daughters[i] = daughter;
 	}
 
 	/**
 	 * @return the content data
 	 */
 	public Object getData() {
-		/* TO BE COMPLETED */
+		return data;
 	}
 
 	/**
 	 * @param data
 	 */
 	public void setData(Object data) {
-		/* TO BE COMPLETED */
+		this->data = data;
 	}
 
 	/**
 	 * @return {@code true} if and only if this node has at least one non-null
-	 *         daughter node.
+	 * daughter node.
 	 */
 	public boolean hasDaughters() {
-		/* TO BE COMPLETED */
+		for (int i = 0; i < NODE_MAX_ARITY; ++i)
+			if (!daughters[i])
+				return true;
+		return false;
 	}
 
 	/* Constructors */
+
 	/**
 	 * Default constructor.
 	 */
@@ -112,11 +127,27 @@ public class NodeMultiple {
 	/**
 	 * Constructor. Sets the content data to {@code data} and creates an empty set
 	 * of daughters.
-	 * 
+	 *
 	 * @param data
 	 */
 	public NodeMultiple(Object data) {
-		/* TO BE COMPLETED */
+		this->data = data;
+	}
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 // eof
